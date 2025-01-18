f14bade7407dd5dae0014ae1fa92df895251d9f4
Korekiwi <foxiknyan@gmail.com>
2025-01-18 18:51:17 +0300

    добавлен пост через shell_plus

--- python_blog/models.py
+++ python_blog/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from unidecode import unidecode
 from django.utils.text import slugify
 
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name="Заголовок")
-    text = models.TextField(verbose_name='Контент')
+    content = models.TextField(verbose_name='Контент')
     slug = models.SlugField(max_length=250, unique=True, verbose_name="Слаг", blank=True, null=True)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name="Автор", related_name="posts", default=None, null=True)
     tags = models.JSONField(null=True, blank=True, default=list)
@@ -19,7 +20,7 @@ class Post(models.Model):
     def get_absolute_url(self):
         return f'/post/{self.slug}/'
 
-    def save(self):
+    def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
+            self.slug = slugify(unidecode(self.title))
         super().save(*args, **kwargs)
