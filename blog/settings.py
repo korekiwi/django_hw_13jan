0fe3d48b1c689563e0953b5ab617973e7480e44a
Korekiwi <foxiknyan@gmail.com>
2025-01-18 17:59:17 +0300

    подключена mysql; создана модель поста в models.py

--- blog/settings.py
+++ blog/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_project',
+        'USER': 'root',
+        'PASSWORD': '29032017',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- python_blog/models.py
+++ python_blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    slug = models.SlugField(unique=True, blank=True)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE())
+    tags = models.JSONField(null=True, blank=True, default=list)
+    published_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return f'/post/{self.slug}/'
+
+    def save(self):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
