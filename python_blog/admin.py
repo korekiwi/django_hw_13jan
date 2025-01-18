d25182e63d6b2ccd5aaa2ba6679f2c86e1de6658
Korekiwi <foxiknyan@gmail.com>
2025-01-18 19:23:47 +0300

    настроена админка

--- python_blog/admin.py
+++ python_blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from python_blog.models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title', 'content']
+    list_display = ["title", "published_date", "updated_date", "category"]
+    readonly_fields = ["slug"]
